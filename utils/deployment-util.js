3582011ab89f6f61b0d8f8dc60e781e220cc3153
lSoonami <109587975+lSoonami@users.noreply.github.com>
2022-08-31 13:26:57 +0100

    Update deployment-util.js

--- utils/deployment-util.js
+++ utils/deployment-util.js
@@ -143,23 +143,14 @@ const createExtensions = async ({ dao, factories, options }) => {
           `Missing deployment argument <${argName}> in ${factoryConfigs.name}.create`
         );
       });
-      tx = await factory.create(...args);
+      await waitTx(factory.create(...args));
     } else {
-      tx = await factory.create();
+      await waitTx(factory.create());
     }
-    /**
-     * The tx event is the safest way to read the new extension address.
-     * Event at index 0 indicates the extension was created
-     * Arg at index 1 represents the new extension address
-     */
-    let extensionAddress;
-    if (tx.wait) {
-      const res = await tx.wait();
-      extensionAddress = res.events[0].args[1];
-    } else {
-      const { logs } = tx;
-      extensionAddress = logs[0].args[1];
-    }
+    const extensionAddress = await factory.getExtensionAddress(
+      options.daoAddress
+    );
+    
     const extensionInterface = options[extensionConfigs.name];
     if (!extensionInterface)
       throw new Error(
